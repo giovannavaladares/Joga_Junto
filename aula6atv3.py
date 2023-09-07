446d250434a948106ce2dfb5f615b873eb71f871
giovaNNa <143637109+giovannavaladares@users.noreply.github.com>
2023-09-07 17:40:18 -0300

    Update aula6atv3.py

--- aula6atv3.py
+++ aula6atv3.py
@@ -1,2 +1,14 @@
-frutas = ['maça', 'uva', 'melancia']
-print(frutas)
+# define frutas aleatorias # 
+
+frutas = ['banana','uva','morango','kiwi','carambola','pera','tomate']
+
+# define frutas alergia # 
+
+alergias = ['banana','uva','tomate']
+
+# resultado final # 
+
+for fruta in frutas: 
+    if fruta in alergias: 
+        print(f'O que você tem alergia, {fruta} está contida na lista') 
+
