4c57e0bf53f63af020939a58d51bc1e3df10d31c
giovaNNa <143637109+giovannavaladares@users.noreply.github.com>
2023-09-07 17:41:05 -0300

    Update aula5atv2.py

--- aula5atv2.py
+++ aula5atv2.py
@@ -1,11 +1,18 @@
-valor_da_compra = float(input("Digite o valor da compra: 70"))
 
+## determina valor da compra ##
+valor_da_compra = float(70)
+
+## se a compra for maior que 250 e menor que 500 ##
 
 if valor_da_compra >= 250 and valor_da_compra < 500:
     print("PARABÉNS. VOCÊ GANHOU 10% DE DESCONTO, MAS PODE GANHAR 30% SE SUA COMPRA FOR ACIMA DE R$500,00")
 
+## se a compra for maior ou igual que 500 ##
+
 elif valor_da_compra >= 500:
     print("PARABÉNS. VOCÊ GANHOU SUPER DESCONTO DE 30%")
+
+ ## se a compra for menor que 500 ##
 
 else:
     print("POXA, FALTA POUCO PARA VOCÊ GANHAR 10% DE DESCONTO EM SUA COMPRA.")
