f776d65088794230e2b1679afc51dabd3ccac6c7
giovaNNa <143637109+giovannavaladares@users.noreply.github.com>
2023-09-10 16:42:12 -0300

    Update aula6atv5.py

--- aula6atv5.py
+++ aula6atv5.py
@@ -1,11 +1,11 @@
-email_check = False 
+email_check = False
 dominio = "jogajuntoinstituto.org"
 
-while email_check == False 
-email = input("Digite o seu e-mail: ")
+while email_check == False:
+    email = input("Digite o seu email: giovannavaladaresz@gmail.com")
 
-if dominio in email: 
-    print("Este é um email jogajunto")
-    email_check = True 
-
-else: print("Este não é um email do instituto")
+    if dominio in email:
+        print("Este é um email joga junto")
+        email_check = True
+    else:
+        print("Este não é um email do instituto")
