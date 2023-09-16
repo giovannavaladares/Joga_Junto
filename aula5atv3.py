9bccaeee63c92a8748428c944272a411e741e95c
giovaNNa <143637109+giovannavaladares@users.noreply.github.com>
2023-09-16 16:57:43 -0300

    Create aula5atv3.py

---
+++ aula5atv3.py
@@ -0,0 +1,11 @@
+frequencia_aluno = int(input("Digite a frequência do aluno:"))
+
+
+if frequencia_aluno >= 2 and frequencia_aluno <= 20:
+    print(f"VOCÊ ESTÁ PARTICIPANDO DA NOSSA PROMO TREINA JUNTO")
+
+elif frequencia_aluno >= 21:
+    print(f"UHUU. AGORA VOCÊ PODE PRESENTEAR UM AMIGO OU AMIGA PARA TREINAR COM VOCÊ")
+
+else:
+    print(f"QUE BOM VER VOCÊ DE VOLTA. A PARTIR DE AGORA INICIAMOS MAIS UMA CONTAGEM DE 21 DIAS PARA A PROMO TREINA JUNTO.")a
