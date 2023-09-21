01da63b165f7ca777b9a3b0cc3bb2c9d6ebbf856
giovaNNa <143637109+giovannavaladares@users.noreply.github.com>
2023-09-21 19:00:43 -0300

    Create aula4atv3.py

---
+++ aula4atv3.py
@@ -0,0 +1,8 @@
+valor = int(input('Digite o valor: '))
+valor_dobro = valor * 2
+valor_triplo = valor * 3
+valor_quadrado = valor **2
+valor_raiz = valor ** (1/2)
+valor_raiz_cubica = valor ** (1/3)
+
+print(f"O dobro do valor inserido: {valor_dobro}, o triplo do valor inserido: {valor_triplo}, o valor ao quadrado: {valor_quadrado}, a raiz quadrada do valor inderido: {valor_raiz}, a raiz cúbica do valor inserido: {valor_raiz_cubica}")
