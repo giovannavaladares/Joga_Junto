bbf4465aa7ee405c2bb0c4a5f78e38d7d947cf72
giovaNNa <143637109+giovannavaladares@users.noreply.github.com>
2023-09-21 19:01:10 -0300

    Create aula4atv4.py

---
+++ aula4atv4.py
@@ -0,0 +1,21 @@
+nome_pet = input('Digite o nome do pet: ')
+idade_pet = int(input('Digite a idade do pet: '))
+anos_cachorro = idade_pet * 7
+
+print(f"Olá, {nome_pet} tem {anos_cachorro} anos de idade.")
+
+
+"""
+Além disso, ela deseja calcular, a cada 12 meses, o lucro obtido por banho e por cachorro. 
+
+VALORES POR BANHO X CUSTO POR BANHO
+
+Cachorro de grande porte: BANHO: R$75,00 | CUSTO: R$20,00
+Cachorro de médio porte: BANHO: R$60,00 | CUSTO: 15,00
+Cachorro de médio porte: BANHO: R$50,00 | CUSTO: R$5,00
+Exemplo: Se um animal de grande porte tomar 10 banhos em 12 meses, no final, o programa deve imprimir a seguinte informação:
+
+Olá, Tuco tem 35 anos e nos últimos 12 meses o lucro com este animal foi de R$550,00
+print(f"Olá, {nome_pet} tem {idade_pet} anos") e nos ultimos 12 meses o lucro com este animal foi de R${}
+
+"""
